9975df44a5ff028bbcf8b648046dd8762682b366
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-13 20:39:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,26 +20,56 @@ from nltk.stem import WordNetLemmatizer
 model = joblib.load('sgd_classifier_model.joblib')
 vectorizer = joblib.load('tfidf_vectorizer.joblib')
 
+import streamlit as st
+import joblib
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-def binary_cyberbullying_detection(text, model, vectorizer):
+# Load the SGD classifier and TF-IDF vectorizer
+sgd_classifier = joblib.load('sgd_classifier_model.joblib')
+tfidf_vectorizer = joblib.load('tfidf_vectorizer.joblib')
+
+# Function to clean and preprocess text
+def preprocess_text(text):
+    text = re.sub(r'http\S+|www\S+|@\S+|#\S+|[^A-Za-z\s]', '', text)
+    text = text.lower()
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in text.split() if word not in stop_words]
+    return ' '.join(tokens)
+
+# Function for binary cyberbullying detection
+def binary_cyberbullying_detection(text):
     try:
         # Preprocess the input text
         preprocessed_text = preprocess_text(text)
 
         # Transform the preprocessed text using the loaded vectorizer
-        text_tfidf = vectorizer.transform([preprocessed_text])
+        text_tfidf = tfidf_vectorizer.transform([preprocessed_text])
 
         # Make prediction
-        prediction = model.predict(text_tfidf)
+        prediction = sgd_classifier.predict(text_tfidf)
 
         return prediction[0]
     except Exception as e:
-        print(f"Error: {e}")
+        st.error(f"Error: {e}")
         return None
 
-# Example usage:
-user_input = "This is a tweet containing cyberbullying language."
-prediction = binary_cyberbullying_detection(user_input, model, vectorizer)
+# Streamlit UI
+st.title('Cyberbullying Detection App')
 
-if prediction is not None:
-    print(f"Prediction: {'Cyberbullying' if prediction == 1 else 'Not Cyberbullying'}")
+# Input text box
+user_input = st.text_area("Enter a text:", "")
+
+# Check if the user has entered any text
+if user_input:
+    # Make prediction
+    prediction = binary_cyberbullying_detection(user_input)
+
+    # Display the prediction
+    if prediction is not None:
+        st.write(f"Prediction: {'Cyberbullying' if prediction == 1 else 'Not Cyberbullying'}")
+
+
