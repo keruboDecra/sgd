31c0056f8a278a88ddc52a0f19493dc07d84f8b9
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-13 21:31:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,21 +47,20 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 nltk.download('stopwords')
 nltk.download('wordnet')
 
-# Load the SGD classifier and TF-IDF vectorizer for binary cyberbullying detection
-sgd_classifier = joblib.load('sgd_classifier_model.joblib')
-tfidf_vectorizer_binary = joblib.load('tfidf_vectorizer.joblib')
+import streamlit as st
+import joblib
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Load the models for multi-class cyberbullying detection
-models = {
-    'Random Forest': joblib.load('random_forest_model.joblib'),
-    'Logistic Regression': joblib.load('logistic_regression_model.joblib'),
-    'Stochastic Gradient Descent': joblib.load('sgd_model.joblib'),
-    'Naive Bayes': joblib.load('naive_bayes_model.joblib'),
-    'Support Vector Machine': joblib.load('svm_model.joblib'),
-}
+# Download NLTK resources
+nltk.download('stopwords')
+nltk.download('wordnet')
 
-# Load the TF-IDF vectorizer for multi-class cyberbullying detection
-tfidf_vectorizer_multiclass = joblib.load('tfidf_vectorizer_multiclass.joblib')
+# Load the entire pipeline (including TfidfVectorizer and SGDClassifier)
+model_pipeline = joblib.load('sgd_classifier_model.joblib')
 
 # Function to clean and preprocess text
 def preprocess_text(text):
@@ -73,48 +72,15 @@ def preprocess_text(text):
     return ' '.join(tokens)
 
 # Function for binary cyberbullying detection
-# Function to predict using the loaded model
 def binary_cyberbullying_detection(text):
     try:
-        # Load the model and vectorizer
-        model = joblib.load('random_forest_model.joblib')
-        vectorizer = joblib.load('tfidf_vectorizer.joblib')
-
         # Preprocess the input text
         preprocessed_text = preprocess_text(text)
 
-        # Transform the preprocessed text using the loaded vectorizer
-        text_tfidf = vectorizer.transform([preprocessed_text])
-
-        # Make prediction
-        prediction = model.predict(text_tfidf)
+        # Make prediction using the loaded pipeline
+        prediction = model_pipeline.predict([preprocessed_text])
 
         return prediction[0]
-    except FileNotFoundError:
-        st.error("Model file not found. Make sure the model file exists.")
-        return None
-    except Exception as e:
-        st.error(f"Error loading or using the model: {e}")
-        return None
-
-
-# Function for multi-class cyberbullying detection
-def multi_class_cyberbullying_detection(text):
-    try:
-        # Preprocess the input text
-        preprocessed_text = preprocess_text(text)
-
-        # Transform the preprocessed text using the loaded vectorizer
-        text_tfidf = tfidf_vectorizer_multiclass.transform([preprocessed_text])
-
-        # Make predictions on the sample text for each model
-        predictions = {}
-        for model_name, model in models.items():
-            prediction_encoded = model.predict(text_tfidf)
-            prediction = label_encoder.inverse_transform(prediction_encoded)
-            predictions[model_name] = prediction[0]
-
-        return predictions
     except Exception as e:
         st.error(f"Error: {e}")
         return None
@@ -127,16 +93,10 @@ user_input = st.text_area("Enter a text:", "")
 
 # Check if the user has entered any text
 if user_input:
-    # Make prediction for binary cyberbullying detection
-    binary_prediction = binary_cyberbullying_detection(user_input)
+    # Make prediction
+    prediction = binary_cyberbullying_detection(user_input)
 
-    # Display the binary prediction
-    st.write(f"Binary Prediction: {'Cyberbullying' if binary_prediction == 1 else 'Not Cyberbullying'}")
+    # Display the prediction
+    if prediction is not None:
+        st.write(f"Prediction: {'Cyberbullying' if prediction == 1 else 'Not Cyberbullying'}")
 
-    # If cyberbullying is detected, make prediction for multi-class cyberbullying detection
-    if binary_prediction == 1:
-        # Make prediction for multi-class cyberbullying detection
-        multi_class_prediction = multi_class_cyberbullying_detection(user_input)
-        
-        # Display the specific category prediction
-        st.write(f"Specific Category: {multi_class_prediction}")
