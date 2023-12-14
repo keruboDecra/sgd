fd0c363ff7196eda79bd9b3c24ab2c2e230709b5
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-14 09:35:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,21 @@ import nltk
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import make_pipeline
-from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 # Download NLTK resources
-nltk.download('stopwords')
 nltk.download('wordnet')
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 # Load the logo image
 logo = Image.open('logo.png')
@@ -28,10 +35,47 @@ def preprocess_text(text):
     tokens = [lemmatizer.lemmatize(word) for word in text.split() if word not in stop_words]
     return ' '.join(tokens)
 
-# Load the default model
-default_model = joblib.load('sgd_classifier_model.joblib')
+# Function for binary cyberbullying detection
+def binary_cyberbullying_detection(text):
+    try:
+        # Preprocess the input text
+        preprocessed_text = preprocess_text(text)
 
-# Streamlit UI
+        # Make prediction using the loaded pipeline
+        prediction = model_pipeline.predict([preprocessed_text])
+
+        # Check for offensive words
+        with open('en.txt', 'r') as f:
+            offensive_words = [line.strip() for line in f]
+
+        offending_words = [word for word in preprocessed_text.split() if word in offensive_words]
+
+        return prediction[0], offending_words
+    except Exception as e:
+        st.error(f"Error in binary_cyberbullying_detection: {e}")
+        return None, None
+
+# Function for multi-class cyberbullying detection
+def multi_class_cyberbullying_detection(text):
+    try:
+        # Preprocess the input text
+        preprocessed_text = preprocess_text(text)
+
+        # Make prediction
+        decision_function_values = sgd_classifier.decision_function([preprocessed_text])[0]
+
+        # Get the predicted class index
+        predicted_class_index = np.argmax(decision_function_values)
+
+        # Get the predicted class label using the label encoder
+        predicted_class_label = label_encoder.inverse_transform([predicted_class_index])[0]
+
+        return predicted_class_label, decision_function_values
+    except Exception as e:
+        st.error(f"Error in multi_class_cyberbullying_detection: {e}")
+        return None
+
+# Set page title and icon
 st.set_page_config(
     page_title="Cyberbullying Detection App",
     page_icon="🕵️",
@@ -99,54 +143,62 @@ st.markdown(
 st.image(logo, caption=None, width=10, use_column_width=True)
 st.title('Cyberbullying Detection App')
 
-# Option to upload a CSV file
-uploaded_file = st.file_uploader("Upload CSV File for Experimentation", type=["csv"])
+# Experimentation page
+if st.button("Experiment"):
+    st.header("Experimentation Page")
+    st.write("Upload your CSV file for training and prediction.")
+    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
-if uploaded_file is not None:
-    # Load the uploaded dataset
-    df_uploaded = pd.read_csv(uploaded_file)
+    if uploaded_file is not None:
+        # Load the dataset
+        df_exp = pd.read_csv(uploaded_file)
 
-    # Display the first few rows of the uploaded dataset
-    st.subheader("First few rows of the uploaded dataset:")
-    st.write(df_uploaded.head())
+        # Display the first few rows of the dataset
+        st.write("First few rows of the uploaded dataset:")
+        st.write(df_exp.head())
 
-    # Preprocess the uploaded data
-    df_uploaded['cleaned_text'] = df_uploaded['tweet_text'].apply(preprocess_text)
+        # Create a new DataFrame for preprocessed data
+        df_preprocessed_exp = df_exp.copy()
 
-    # Encode the target variable
-    df_uploaded['encoded_label'] = label_encoder.transform(df_uploaded['cyberbullying_type'])
+        # Apply text preprocessing to the 'tweet_text' column
+        df_preprocessed_exp['cleaned_text'] = df_preprocessed_exp['tweet_text'].apply(preprocess_text)
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(
-        df_uploaded['cleaned_text'],
-        df_uploaded['encoded_label'],
-        test_size=0.2,
-        random_state=42
-    )
+        # Display the cleaned and preprocessed data
+        st.write("Cleaned and Preprocessed Data:")
+        st.write(df_preprocessed_exp[['tweet_text', 'cleaned_text']].head())
 
-    # Display the shapes of the training and testing sets
-    st.subheader("Shapes of Training and Testing Sets for the Uploaded Dataset:")
-    st.write(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
-    st.write(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
+        # Encode the target variable
+        df_preprocessed_exp['encoded_label'] = label_encoder.transform(df_preprocessed_exp['cyberbullying_type'])
 
-    # Train the model on the uploaded dataset
-    model_pipeline_uploaded = make_pipeline(TfidfVectorizer(), SGDClassifier(random_state=42))
-    model_pipeline_uploaded.fit(X_train, y_train)
+        # Display the encoded labels
+        st.write("Encoded Labels:")
+        st.write(df_preprocessed_exp[['cyberbullying_type', 'encoded_label']].head())
 
-    # Make predictions on the test data
-    y_pred_uploaded = model_pipeline_uploaded.predict(X_test)
+        # Split the data into training and testing sets
+        X_train_exp, X_test_exp, y_train_exp, y_test_exp = train_test_split(
+            df_preprocessed_exp['cleaned_text'],
+            df_preprocessed_exp['encoded_label'],
+            test_size=0.2,
+            random_state=42
+        )
 
-    # Evaluate the model
-    accuracy_uploaded = accuracy_score(y_test, y_pred_uploaded)
-    st.subheader(f"Accuracy on the Uploaded Dataset: {accuracy_uploaded:.2f}")
+        # Display the shapes of the training and testing sets
+        st.write("Shapes of Training and Testing Sets:")
+        st.write(f"X_train shape: {X_train_exp.shape}, y_train shape: {y_train_exp.shape}")
+        st.write(f"X_test shape: {X_test_exp.shape}, y_test shape: {y_test_exp.shape}")
 
-    # Display additional evaluation metrics
-    st.subheader("Classification Report on the Uploaded Dataset:")
-    st.write(classification_report(y_test, y_pred_uploaded))
+        # Train and evaluate each model on the user's dataset
+        for model_name, model in models.items():
+            # Train the model
+            model.fit(X_train_exp, y_train_exp)
 
-    # Option to save the trained model
-    save_model = st.checkbox("Save the Trained Model")
-    if save_model:
-        # Save the model pipeline to a file
-        joblib.dump(model_pipeline_uploaded, 'uploaded_model.joblib', protocol=4)
-        st.success("Trained model saved as 'uploaded_model.joblib'")
+            # Make predictions on the test data
+            y_pred_exp = model.predict(X_test_exp)
+
+            # Evaluate the model
+            accuracy_exp = accuracy_score(y_test_exp, y_pred_exp)
+            st.write(f"\n{model_name} Accuracy on User's Dataset: {accuracy_exp:.2f}")
+
+            # Display additional evaluation metrics
+            st.write("Classification Report:")
+            st.write(classification_report(y_test_exp, y_pred_exp))
