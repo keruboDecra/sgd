8fc7f097ea52d484c965eae5a372dd93eb4d64fb
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-14 11:04:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def experiment_with_dataset():
         model_pipeline.fit(X_train_new, y_train_new)
 
         # Save the updated pipeline to the original file path
-        joblib.dump(model_pipeline, '/content/drive/My Drive/sgb/sgd_classifier_model.joblib', protocol=4)
+        joblib.dump(model_pipeline, 'sgd_classifier_model.joblib', protocol=4)
 
         # Optional: Print or return any relevant information
         st.success("Dataset reprocessed and model retrained successfully.")
