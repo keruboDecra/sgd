9dce27c9ad04516f0c9bfbf351640af20d69c720
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-14 11:34:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,55 +74,51 @@ def multi_class_cyberbullying_detection(text):
 
 # Function to load custom dataset, preprocess, and train the model
 def experiment_with_dataset():
-
-    # Ask the user to upload a file
-    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
     st.write("Experiment function is executing!")
 
-    if uploaded_file is not None:
-        
-        st.write(f"File uploaded: {uploaded_file.name}")
-        st.write("Uploaded successfully!")
+    # Button to trigger dataset upload
+    if st.button("Upload Dataset"):
+        # Ask the user to upload a file
+        uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
+        if uploaded_file is not None:
+            st.write(f"File uploaded: {uploaded_file.name}")
 
-        # Load the new dataset
-        df_new = pd.read_csv(uploaded_file)
-        st.write("These are the elements of your data!")
+            # Load the new dataset
+            df_new = pd.read_csv(uploaded_file)
 
+            # Create a new DataFrame for preprocessed data
+            df_preprocessed_new = df_new.copy()
 
-        # Create a new DataFrame for preprocessed data
-        df_preprocessed_new = df_new.copy()
+            # Apply text preprocessing to the 'tweet_text' column
+            df_preprocessed_new['cleaned_text'] = df_preprocessed_new['tweet_text'].apply(preprocess_text)
 
-        # Apply text preprocessing to the 'tweet_text' column
-        df_preprocessed_new['cleaned_text'] = df_preprocessed_new['tweet_text'].apply(preprocess_text)
+            # Display the processed DataFrame
+            st.write("Processed DataFrame:")
+            st.write(df_preprocessed_new.head())
 
-        # Display the processed DataFrame
-        st.write("Processed DataFrame:")
-        st.write(df_preprocessed_new.head())
+            # Encode the target variable using the saved label encoder
+            df_preprocessed_new['encoded_label'] = label_encoder.transform(df_preprocessed_new['cyberbullying_type'])
 
-        # Encode the target variable using the saved label encoder
-        df_preprocessed_new['encoded_label'] = label_encoder.transform(df_preprocessed_new['cyberbullying_type'])
+            # Split the new data into training and testing sets
+            X_train_new, X_test_new, y_train_new, y_test_new = train_test_split(
+                df_preprocessed_new['cleaned_text'],
+                df_preprocessed_new['encoded_label'],
+                test_size=0.2,
+                random_state=42
+            )
 
-        # Split the new data into training and testing sets
-        X_train_new, X_test_new, y_train_new, y_test_new = train_test_split(
-            df_preprocessed_new['cleaned_text'],
-            df_preprocessed_new['encoded_label'],
-            test_size=0.2,
-            random_state=42
-        )
+            st.write("New dataset preprocessed.")
 
-        st.write("New dataset preprocessed.")
+            # Retrain the model on the new training data
+            model_pipeline.fit(X_train_new, y_train_new)
 
-        # Retrain the model on the new training data
-        model_pipeline.fit(X_train_new, y_train_new)
+            st.write("Model retrained.")
 
-        st.write("Model retrained.")
+            # Save the updated pipeline to the original file path
+            joblib.dump(model_pipeline, 'sgd_classifier_model.joblib', protocol=4)
 
-        # Save the updated pipeline to the original file path
-        joblib.dump(model_pipeline, 'sgd_classifier_model.joblib', protocol=4)
-
-        st.success("Dataset reprocessed and model retrained successfully.")
-
+            st.success("Dataset reprocessed and model retrained successfully.")
 # Rest of your code...
 
 
