0d80a5da7814c709cc96a08df3f87ac4ce35c892
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-14 09:27:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ import pandas as pd
 import nltk
 from PIL import Image
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 # Download NLTK resources
-nltk.download('wordnet')
 nltk.download('stopwords')
-
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
+nltk.download('wordnet')
 
 # Load the logo image
 logo = Image.open('logo.png')
@@ -29,47 +28,10 @@ def preprocess_text(text):
     tokens = [lemmatizer.lemmatize(word) for word in text.split() if word not in stop_words]
     return ' '.join(tokens)
 
-# Function for binary cyberbullying detection
-def binary_cyberbullying_detection(text):
-    try:
-        # Preprocess the input text
-        preprocessed_text = preprocess_text(text)
+# Load the default model
+default_model = joblib.load('sgd_classifier_model.joblib')
 
-        # Make prediction using the loaded pipeline
-        prediction = model_pipeline.predict([preprocessed_text])
-
-        # Check for offensive words
-        with open('en.txt', 'r') as f:
-            offensive_words = [line.strip() for line in f]
-
-        offending_words = [word for word in preprocessed_text.split() if word in offensive_words]
-
-        return prediction[0], offending_words
-    except Exception as e:
-        st.error(f"Error in binary_cyberbullying_detection: {e}")
-        return None, None
-
-# Function for multi-class cyberbullying detection
-def multi_class_cyberbullying_detection(text):
-    try:
-        # Preprocess the input text
-        preprocessed_text = preprocess_text(text)
-
-        # Make prediction
-        decision_function_values = sgd_classifier.decision_function([preprocessed_text])[0]
-
-        # Get the predicted class index
-        predicted_class_index = np.argmax(decision_function_values)
-
-        # Get the predicted class label using the label encoder
-        predicted_class_label = label_encoder.inverse_transform([predicted_class_index])[0]
-
-        return predicted_class_label, decision_function_values
-    except Exception as e:
-        st.error(f"Error in multi_class_cyberbullying_detection: {e}")
-        return None
-
-# Set page title and icon
+# Streamlit UI
 st.set_page_config(
     page_title="Cyberbullying Detection App",
     page_icon="🕵️",
@@ -137,80 +99,54 @@ st.markdown(
 st.image(logo, caption=None, width=10, use_column_width=True)
 st.title('Cyberbullying Detection App')
 
-# Upload a CSV file for training or prediction
-uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
+# Option to upload a CSV file
+uploaded_file = st.file_uploader("Upload CSV File for Experimentation", type=["csv"])
 
 if uploaded_file is not None:
-    st.write("File Uploaded Successfully!")
+    # Load the uploaded dataset
+    df_uploaded = pd.read_csv(uploaded_file)
 
-    # Load the uploaded CSV file
-    uploaded_df = pd.read_csv(uploaded_file)
+    # Display the first few rows of the uploaded dataset
+    st.subheader("First few rows of the uploaded dataset:")
+    st.write(df_uploaded.head())
 
-    # Display the uploaded dataset
-    st.write("Uploaded Dataset:")
-    st.write(uploaded_df)
+    # Preprocess the uploaded data
+    df_uploaded['cleaned_text'] = df_uploaded['tweet_text'].apply(preprocess_text)
 
-    # Example: Split the data for training and testing
+    # Encode the target variable
+    df_uploaded['encoded_label'] = label_encoder.transform(df_uploaded['cyberbullying_type'])
+
+    # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(
-        uploaded_df['text'], uploaded_df['label'], test_size=0.2, random_state=42
+        df_uploaded['cleaned_text'],
+        df_uploaded['encoded_label'],
+        test_size=0.2,
+        random_state=42
     )
 
-    # Example: Train a simple model (replace this with your actual training logic)
-    # model = YourModel()
-    # model.fit(X_train, y_train)
+    # Display the shapes of the training and testing sets
+    st.subheader("Shapes of Training and Testing Sets for the Uploaded Dataset:")
+    st.write(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
+    st.write(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
 
-    # Example: Make predictions on the test set
-    # predictions = model.predict(X_test)
+    # Train the model on the uploaded dataset
+    model_pipeline_uploaded = make_pipeline(TfidfVectorizer(), SGDClassifier(random_state=42))
+    model_pipeline_uploaded.fit(X_train, y_train)
 
-    # Example: Display accuracy score (replace this with your actual evaluation logic)
-    # accuracy = accuracy_score(y_test, predictions)
-    # st.write(f"Accuracy: {accuracy}")
+    # Make predictions on the test data
+    y_pred_uploaded = model_pipeline_uploaded.predict(X_test)
 
-    # You can use the uploaded data for fine-tuning the model or making predictions as needed
-    # ...
+    # Evaluate the model
+    accuracy_uploaded = accuracy_score(y_test, y_pred_uploaded)
+    st.subheader(f"Accuracy on the Uploaded Dataset: {accuracy_uploaded:.2f}")
 
-# Input text box
-user_input = st.text_area("Share your thoughts:", "", key="user_input")
+    # Display additional evaluation metrics
+    st.subheader("Classification Report on the Uploaded Dataset:")
+    st.write(classification_report(y_test, y_pred_uploaded))
 
-# Button to trigger analysis
-analyze_button = st.button("Analyze")
-
-# View flag for detailed predictions
-view_predictions = st.checkbox("View Detailed Predictions", value=False)
-
-# Check if the user has entered any text and the button is clicked
-if user_input and analyze_button:
-    # Make binary prediction and check for offensive words
-    binary_result, offensive_words = binary_cyberbullying_detection(user_input)
-    st.markdown("<div class='st-bw'>", unsafe_allow_html=True)
-    
-    if view_predictions:
-        st.write(f"Binary Cyberbullying Prediction: {'Cyberbullying' if binary_result == 1 else 'Not Cyberbullying'}")
-
-    # Display offensive words and provide recommendations
-    if offensive_words and view_predictions:
-        st.warning(f"While this tweet is not necessarily cyberbullying, it may contain offensive language. Consider editing. Detected offensive words: {offensive_words}")
-
-    st.markdown("</div>", unsafe_allow_html=True)
-
-    # Make multi-class prediction
-    multi_class_result = multi_class_cyberbullying_detection(user_input)
-    if multi_class_result is not None:
-        predicted_class, prediction_probs = multi_class_result
-        st.markdown("<div class='st-eb'>", unsafe_allow_html=True)
-        
-        if view_predictions:
-            st.write(f"Multi-Class Predicted Class: {predicted_class}")
-
-        st.markdown("</div>", unsafe_allow_html=True)
-
-        # Check if classified as cyberbullying
-        if predicted_class != 'not_cyberbullying':
-            st.error(f"Please edit your tweet before resending, your text contains content that may appear as bullying to other users. {predicted_class.replace('_', ' ').title()}.")
-
-        elif offensive_words and not view_predictions:
-            st.warning("While this tweet is not necessarily cyberbullying, it may contain offensive language. Consider editing.")
-        else:
-            # Button to send tweet
-            if st.button('Send Tweet'):
-                st.success('Tweet Sent! This tweet is safe to send.')
+    # Option to save the trained model
+    save_model = st.checkbox("Save the Trained Model")
+    if save_model:
+        # Save the model pipeline to a file
+        joblib.dump(model_pipeline_uploaded, 'uploaded_model.joblib', protocol=4)
+        st.success("Trained model saved as 'uploaded_model.joblib'")
