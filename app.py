9bc4dd9b8425dd9c829e85aef57ffe948736a3ce
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-15 11:22:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import nltk
 from PIL import Image
-from sklearn.model_selection import train_test_split
 
 # Download NLTK resources
 nltk.download('wordnet')
@@ -16,22 +15,15 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 
-# Load the logo image
-logo = Image.open('logo.png')
-
-# Define the SessionState class
-class SessionState:
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-# Create a session state object
-session_state = SessionState(predictions={'binary_result': None, 'offensive_words': None, 'multi_class_result': None})
-
-# Load the pre-trained pipeline
+# Load the entire pipeline (including TfidfVectorizer and SGDClassifier)
 model_pipeline = joblib.load('sgd_classifier_model.joblib')
+
+# Load the SGD classifier, TF-IDF vectorizer, and label encoder
 sgd_classifier = joblib.load('sgd_classifier_model.joblib')
 label_encoder = joblib.load('label_encoder.joblib')
+
+# Load the logo image
+logo = Image.open('logo.png')
 
 # Function to clean and preprocess text
 def preprocess_text(text):
@@ -81,81 +73,6 @@ def multi_class_cyberbullying_detection(text):
     except Exception as e:
         st.error(f"Error in multi_class_cyberbullying_detection: {e}")
         return None
-
-
-
-def experiment_with_dataset(uploaded_file):
-# Load the new dataset
-    df_new = pd.read_csv(uploaded_file)
-
-    # Create a new DataFrame for preprocessed data
-    df_preprocessed_new = df_new.copy()
-
-    # Apply text preprocessing to the 'tweet_text' column
-    df_preprocessed_new['cleaned_text'] = df_preprocessed_new['tweet_text'].apply(preprocess_text)
-
-    # Encode the target variable using the saved label encoder
-    df_preprocessed_new['encoded_label'] = label_encoder.transform(df_preprocessed_new['cyberbullying_type'])
-
-    # Split the new data into training and testing sets
-    X_train_new, X_test_new, y_train_new, y_test_new = train_test_split(
-        df_preprocessed_new['cleaned_text'],
-        df_preprocessed_new['encoded_label'],
-        test_size=0.2,
-        random_state=42
-    )
-
-    # Retrain the model on the new training data
-    model_pipeline.fit(X_train_new, y_train_new)
-
-    # Save the updated pipeline to the original file path
-    joblib.dump(model_pipeline, 'sgd_classifier_model.joblib', protocol=4)
-
-    # Optional: Print or return any relevant information
-    st.success("Dataset reprocessed and model retrained successfully.")
-
-
-# @st.cache(allow_output_mutation=True)
-# def experiment_with_dataset(uploaded_file):
-#     try:
-#         print("Start of retrain_model")
-
-#         if uploaded_file is not None:
-#             st.write(f"File uploaded: {uploaded_file.name}")
-
-#             # Load the new dataset
-#             df_new = pd.read_csv(uploaded_file)
-
-#             # Create a new DataFrame for preprocessed data
-#             df_preprocessed_new = df_new.copy()
-
-#             # Apply text preprocessing to the 'tweet_text' column
-#             df_preprocessed_new['cleaned_text'] = df_preprocessed_new['tweet_text'].apply(preprocess_text)
-
-#             # Encode the target variable using the saved label encoder
-#             df_preprocessed_new['encoded_label'] = label_encoder.transform(df_preprocessed_new['cyberbullying_type'])
-
-#             # Split the new data into training and testing sets
-#             X_train_new, X_test_new, y_train_new, y_test_new = train_test_split(
-#                 df_preprocessed_new['cleaned_text'],
-#                 df_preprocessed_new['encoded_label'],
-#                 test_size=0.2,
-#                 random_state=42
-#             )
-
-#             # Retrain the model on the new training data
-#             model_pipeline.fit(X_train_new, y_train_new)
-
-#             # Save the updated pipeline to the original file path
-#             joblib.dump(model_pipeline, 'sgd_classifier_model.joblib', protocol=4)
-
-#             # Optional: Print or return any relevant information
-#             st.success("Dataset reprocessed and model retrained successfully.")
-
-#         print("End of retrain_model")
-#     except Exception as e:
-#         st.error(f"Error in retrain_model: {e}")
-#         print(f"Error in retrain_model: {e}")
 
 # Set page title and icon
 st.set_page_config(
@@ -222,71 +139,41 @@ st.markdown(
 )
 
 # Streamlit UI
-st.sidebar.image(logo, caption=None, width=10, use_column_width=True)
-page = st.sidebar.radio("Select Page", ["Twitter Interaction", "Custom Twitter Interaction"])
+st.image(logo, caption=None, width=10, use_column_width=True)
+st.title('Cyberbullying Detection App')
 
-if page == "Twitter Interaction":
-    st.title('Cyberbullying Detection App')
+# Input text box
+user_input = st.text_area("Share your thoughts:", "", key="user_input")
 
-    # Input text box
-    user_input = st.text_area("Share your thoughts:", "", key="user_input")
+# View flag for detailed predictions
+view_predictions = st.checkbox("View Detailed Predictions", value=False)
 
-    # Button to trigger analysis
-    analyze_button = st.button("Analyze")
+# Check if the user has entered any text
+if user_input:
+    # Make binary prediction and check for offensive words
+    binary_result, offensive_words = binary_cyberbullying_detection(user_input)
+    st.markdown("<div class='st-bw'>", unsafe_allow_html=True)
+    st.write(f"Binary Cyberbullying Prediction: {'Cyberbullying' if binary_result == 1 else 'Not Cyberbullying'}")
 
-    # View flag for detailed predictions
-    view_predictions = st.checkbox("View Detailed Predictions", value=False)
+    # Display offensive words and provide recommendations
+    if offensive_words and view_predictions:
+        st.warning("Offensive words detected! Please consider editing the following words:")
+        st.write(offensive_words)
 
-    # Check if the user has entered any text and the button is clicked
-    if user_input and analyze_button:
-        # Make binary prediction and check for offensive words
-        binary_result, offensive_words = binary_cyberbullying_detection(user_input)
-        session_state.predictions['binary_result'] = binary_result
-        session_state.predictions['offensive_words'] = offensive_words
-        st.markdown("<div class='st-bw'>", unsafe_allow_html=True)
+    st.markdown("</div>", unsafe_allow_html=True)
 
-        if view_predictions:
-            st.write(f"Binary Cyberbullying Prediction: {'Cyberbullying' if binary_result == 1 else 'Not Cyberbullying'}")
-
-        # Display offensive words and provide recommendations
-        if offensive_words and view_predictions:
-            st.warning(f"While this tweet is not necessarily cyberbullying, it may contain offensive language. Consider editing. Detected offensive words: {offensive_words}")
-
+    # Make multi-class prediction
+    multi_class_result = multi_class_cyberbullying_detection(user_input)
+    if multi_class_result is not None:
+        predicted_class, prediction_probs = multi_class_result
+        st.markdown("<div class='st-eb'>", unsafe_allow_html=True)
+        st.write(f"Multi-Class Predicted Class: {predicted_class}")
         st.markdown("</div>", unsafe_allow_html=True)
 
-        # Make multi-class prediction
-        multi_class_result = multi_class_cyberbullying_detection(user_input)
-        session_state.predictions['multi_class_result'] = multi_class_result
-        if multi_class_result is not None:
-            predicted_class, prediction_probs = multi_class_result
-            st.markdown("<div class='st-eb'>", unsafe_allow_html=True)
-
-            if view_predictions:
-                st.write(f"Multi-Class Predicted Class: {predicted_class}")
-
-            st.markdown("</div>", unsafe_allow_html=True)
-
-            # Check if classified as cyberbullying
-            if predicted_class != 'not_cyberbullying':
-                st.error(f"Please edit your tweet before resending. Your text contains content that may appear as bullying to other users. {predicted_class.replace('_', ' ').title()}.")
-            elif offensive_words and not view_predictions:
-                st.warning("While this tweet is not necessarily cyberbullying, it may contain offensive language. Consider editing.")
-            else:
-                # Display message before sending
-                st.success('This tweet is safe to send.')
-
-                # Button to send tweet
-                if st.button('Send Tweet'):
-                    st.success('Tweet Sent!')
-
-elif page == "Custom Twitter Interaction":
-    st.title('Custom Cyberbullying Interaction')
-    # Button to experiment with a custom dataset
-    if st.button("Experiment with Your Dataset"):
-        uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-        df_new = pd.read_csv(uploaded_file)
-
-        experiment_with_dataset(uploaded_file)
-
-        # Mark the function as executed in session_state
-        st.session_state.experiment_executed = True
+        # Check if classified as cyberbullying
+        if predicted_class != 'not_cyberbullying':
+            st.error("Cyberbullying detected! Please edit your tweet before resending.")
+        else:
+            # Button to send tweet
+            if st.button('Send Tweet'):
+                st.success('Tweet Sent! This tweet is safe to send.')
