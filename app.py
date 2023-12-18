b5156a5045ae0023c9ee0a6d32ebb0205ee8fdf2
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-18 16:09:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import nltk
 from PIL import Image
 import sys  # Import the sys module
-from langdetect import detect
+import langid
 
 # Download NLTK resources
 nltk.download('wordnet')
