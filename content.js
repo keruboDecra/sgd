db7d1c53b5cc704ee061d4b7a9f725cab39f9228
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2024-02-04 14:26:22 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
+
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('Content Script Received Message:', request);
+  
   if (request.action === 'detectCyberbullying') {
     var selectedText = window.getSelection().toString();
+    console.log('Selected Text:', selectedText);
     
     // Send the selected text to your Streamlit app for classification
     chrome.runtime.sendMessage({ action: 'classifyText', text: selectedText });
   }
 });
-
