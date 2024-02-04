84b7c1e007f95992ae788a16c87779ff46e6a366
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2024-02-04 18:36:25 +0200

    Update openaitest.py

--- openaitest.py
+++ openaitest.py
@@ -1,6 +1,6 @@
-from openai import OpenAPI
+from openai import OpenAI
 tweet = "nigga what is the blac tace made for, nothing"
-client = OpenAPI(api_key = "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")
+client = OpenAI(api_key = "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")
 chatmessage = client.chat.completions.create(model = "gpt-3.5 turbo",
 messages = [
 {
