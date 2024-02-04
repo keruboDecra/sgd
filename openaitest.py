93510236885266b3cf37c57974279ebae0fe4f63
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2024-02-04 18:32:47 +0200

    Create openaitest.py

---
+++ openaitest.py
@@ -0,0 +1,15 @@
+from openai import OpenAPI
+tweet = "nigga what is the blac tace made for, nothing"
+client = OpenAPI(api_key = "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")
+chatmessage = client.chat.completions.create(model = "gpt-3.5 turbo",
+messages = [
+{
+"role": "user",
+"content": f"Does this content contain cyberbullying elements?, reply with Yes or No:{tweet}"
+
+}
+]
+
+)
+
+print(chatmessage.message.content)
