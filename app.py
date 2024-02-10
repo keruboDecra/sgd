8e6776f51eda1719aa319711cd6f205cd8556d1c
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2024-02-10 22:48:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ label_encoder = joblib.load('label_encoder.joblib')
 
 # Load the logo image
 logo = Image.open('logo.png')
-session_state = session_state(user_input='', chrome_extension_message=None)
+session_state.user_input = ''
+session_state.chrome_extension_message = None
 
 # Function to clean and preprocess text
 def preprocess_text(text):
